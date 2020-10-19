b9ca49bc8ea813ed9a19d6a0f19660ae47803a65
Riccardo <rbrb914@gmail.com>
2020-10-19 19:08:48 +0200

    kekthis/kekthat triggered with highlight

--- kekbot.py
+++ kekbot.py
@@ -16,8 +16,8 @@ bot = commands.Bot(
 async def event_ready():
     'Called once when the bot goes online.'
     print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me is online!")
+    # ws = bot._ws  # this is only needed to send messages within event_ready
+    # await ws.send_privmsg(os.environ['CHANNEL'], f"/me is online!")
 
 
 @bot.command(name='kekwho')
@@ -25,24 +25,39 @@ async def test(ctx):
     await ctx.send('Hello there :) I am Kekbot. I transform Twitch emotes into shitty braille art. '
                    'Type !kekhow and learn how to use me')
 
+
 @bot.command(name='kekhow')
 async def test(ctx):
-    await ctx.send('Type !kekthis or !kekthat followed by the url of an emote. Sometimes '
-                   'it works and sometimes it sucks but I always try my best B). If you want to know more check'
-                   'this out https://github.com/RiccardoBarb/Kekbot')
+    await ctx.send('Use a message highlight to type !kekthis or !kekthat followed by the url of an emote.'
+                   ' Look here for an example: https://github.com/RiccardoBarb/Kekbot')
+
 
 @bot.command(name='kekthis')
 async def kekthis(ctx):
-    command_and_link = ctx.content
-    message_to_chat = kekfunc.handle_request(command_and_link)
-    await ctx.send(message_to_chat)
+    if 'msg-id' in ctx.message.tags and ctx.message.tags['msg-id'] == 'highlighted-message':
+        command_and_link = ctx.content
+        message_to_chat = kekfunc.handle_request(command_and_link)
+        await ctx.send(message_to_chat)
+    else:
+        pass
 
 
 @bot.command(name='kekthat')
 async def kekthis(ctx):
-    command_and_link = ctx.content
-    message_to_chat = kekfunc.handle_request(command_and_link)
-    await ctx.send(message_to_chat)
+    if 'msg-id' in ctx.message.tags and ctx.message.tags['msg-id'] == 'highlighted-message':
+        command_and_link = ctx.content
+        message_to_chat = kekfunc.handle_request(command_and_link)
+        await ctx.send(message_to_chat)
+    else:
+        pass
+
+
+@bot.command(name='test')
+async def test(ctx):
+    if 'msg-id' in ctx.message.tags and ctx.message.tags['msg-id'] == 'highlighted-message':
+        await ctx.send('the message is highlighted')
+    else:
+        pass
 
 
 if __name__ == "__main__":
