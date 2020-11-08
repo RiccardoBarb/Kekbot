4d0282b2c25ae2b5c252966f82679c44fa608515
Riccardo <rbrb914@gmail.com>
2020-11-08 19:37:41 +0100

    removed untracked files

--- utils/database/create_log_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE kekbot.chat_log (
-    author_id INT NOT NULL,
-    author_name VARCHAR(25) NOT NULL,
-    is_mod VARCHAR(5) NOT NULL,
-    is_sub BOOLEAN NOT NULL,
-    is_turbo BOOLEAN NOT NULL,
-    message VARCHAR(500) NULL,
-    channel VARCHAR(25) NOT NULL,
-    message_time TIMESTAMP NOT NULL);
